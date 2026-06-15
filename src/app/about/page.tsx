7de800820ad7c82591f1aef7d91e6d61c854dc92
aikeruiclean-ship-it <aikeruiclean@gmail.com>
2026-06-15 17:25:04 +0800

    fix: remove stray brace in about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -194,7 +194,6 @@ export default function AboutPage() {
         </div>
       </section>
 
-      {
       {/* ===== CUSTOMER SUCCESS ===== */}
       <section className="py-20 bg-white">
         <div className="max-w-7xl mx-auto px-4">
